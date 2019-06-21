7ee1caed17614f3781506593e7d06a9411098f8b
vishalavalani <vishal.avalani@gmail.com>
2019-06-21 19:11:18 +0530

    Final configuration and test styled component in react native

--- route-design-system/src/components/Button/Button.native.js
+++ route-design-system/src/components/Button/Button.native.js
@@ -1,5 +1,26 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
+// import styled from "styled-components/native";
+// import AppStyles from "../config/theme";
+
+// const StyledButton = styled(TouchableOpacity)`
+//   background-color: ${AppStyles.color.BUTTON};
+//   border-color: #0b64eb;
+//   color: #fff;
+//   border-radius: none;
+//   text-align: center;
+//   vertical-align: middle;
+//   border: 1px solid transparent;
+//   padding: 0.2rem 0.5rem;
+//   font-size: 1rem;
+//   line-height: 1.5;
+//   border-radius: 0;
+//   outline: none;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: #4685e4;
+//   }
+// `;
 
 class Button extends React.Component {
   render() {
